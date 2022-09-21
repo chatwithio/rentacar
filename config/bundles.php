1309bcd8dbe6ef72b466e7ac653ee72037578310
hamzaawan7 <saladinian@gmail.com>
2022-09-21 23:33:30 +0500

    some changes to code structure for authentication

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,51 +20,25 @@ class LoginController extends AbstractController
         $this->em = $em;
     }
 
-    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function index(
-        AuthenticationUtils $authenticationUtils,
-        Request $request,    
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('_username');
-            $password = $request->request->get('_password');
-            $user = $userRepository->findBy(['email' => $email]);
-
-            if (!$user) {
-                throw new AuthenticationException('Email/Password wrong.');
-            }
-
-            if (!$passwordHasher->isPasswordValid($user[0], $password)) {
-                throw new AuthenticationException('Email/Password wrong.');
-            }
-
-            return $this->redirectToRoute('messages');
-        }
-
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
     }
 
-
     #[Route('/', name: 'app_home')]
     public function home(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->isGranted('ROLE_USER')){
-            return $this->redirectToRoute('app_upload');
+            return $this->redirectToRoute('messages');
         }
-        return $this->redirectToRoute('app_login');
 
+        return $this->redirectToRoute('app_login');
     }
 }
